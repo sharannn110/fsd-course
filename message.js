9689f18112b32b75576f53d43761dd19536faabe
sharannn110 <sharansai612@gmail.com>
2025-02-11 15:41:09 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+function Message() {
+    return(
+        <div id="container">
+        <h1> hello hii madhav </h1>
+        </div>
+
+    );
+        
+
+
+}
+export default Message;
